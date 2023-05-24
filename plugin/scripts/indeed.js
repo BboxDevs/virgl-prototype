17e5a5cbfbceaef88cecf4f796ccc5fd49cfe315
jekiquin <jci.dev.0@gmail.com>
2023-05-23 18:21:41 -0700

    grabbed job description

--- plugin/scripts/indeed.js
+++ plugin/scripts/indeed.js
@@ -1,19 +1,37 @@
 (() => {
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        console.log('received', obj)
-    
+    chrome.runtime.onMessage.addListener((obj, _sender, _response) => {    
         const urlParams = new URLSearchParams(window.location.search);
         const jobNumber = urlParams.get('vjk') ?? urlParams.get('jk');
     
         const jobTitle = document.querySelector('.jobsearch-JobInfoHeader-title')?.textContent
         const jobCompany = document.querySelector('.jobsearch-CompanyInfoContainer a:first-child')?.textContent
+        const jobDescription = document.querySelector('#jobDescriptionText')?.textContent
+        const indeedApplyButtonWrapper = document.querySelector('#jobsearch-ViewJobButtons-container')
+        const indeedApplyButton = document.querySelector('.jobsearch-ButtonContainer-inlineBlock');
+        const indeedSaveButton = document.querySelector('#saveJobButtonContainer')
+
+        const existingSaveButton = document.querySelector('.virgl-added-button');
+        if (existingSaveButton) {
+            indeedApplyButtonWrapper.removeChild(existingSaveButton);
+        }
+
+        const saveButton = document.createElement('button');
+        saveButton.classList.add('virgl-added-button')
+        indeedApplyButtonWrapper.append(saveButton)
+        saveButton.textContent = "Virgl Save"
+        saveButton.style.setProperty('order', '2')
+        indeedApplyButton.style.setProperty('order', '1');
+        indeedSaveButton.style.setProperty('order', '3')
     
         // todo: the jobTitle and jobCompany doesn't seem to show up on the dashboard. Coz of React
     
-        console.log({
-            jobNumber,
-            jobTitle,
-            jobCompany
+        saveButton.addEventListener('click', () => {
+            console.log('receive message from extension', obj)
+            console.log('saving details:', {
+                jobNumber,
+                jobTitle,
+                jobCompany,jobDescription
+            })
         })
     })
 })()
